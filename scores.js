c6edaeb3e51ea84e6a22f6f700c68351c3325061
Eric Hebert <ayebear@gmail.com>
2017-04-03 16:55:26 -0400

    Styled main page

--- scores.js
+++ scores.js
@@ -2,12 +2,6 @@ function showScores() {
 	let table = document.getElementById("scores")
 	let scores = JSON.parse(localStorage.getItem("highScores")) || {}
 
-	scores["TestA"] = 100
-	scores["TestB"] = 2
-	scores["TestC"] = 3
-	console.log(scores)
-	localStorage.setItem("highScores", JSON.stringify(scores))
-
 	for (let name in scores) {
 		let score = scores[name]
 		let row = table.insertRow(-1)
